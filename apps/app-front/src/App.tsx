0603fb99b6f69b08ef80be0b085bb5a44ba3eae3
Ugo PRENAT <ugo.prenat@edu.itescia.fr>
2023-11-24 19:49:10 +0100

    setup ✨

--- apps/app-front/src/App.tsx
+++ apps/app-front/src/App.tsx
@@ -1,5 +1,12 @@
+import { APP_FRONT_URL } from 'constants';
+
 const App = () => {
-  return <p>app-front</p>;
+  return (
+    <>
+      <p>app-front</p>
+      <p>url: {APP_FRONT_URL}</p>
+    </>
+  );
 };
 
 export default App;
---
+++ packages/constants/src/constants.ts
@@ -0,0 +1,2 @@
+export const APP_FRONT_URL = 'https://app.stats-station.com';
+export const APP_FRONT_DEV_URL = 'http://localhost:5173';
