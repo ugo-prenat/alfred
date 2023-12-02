96819a34e1e9522f929e4b445cd853b77b338f4f
Ugo PRENAT <ugo.prenat@edu.itescia.fr>
2023-12-02 17:46:26 +0100

    feat: app-front: basic Twitch auth

--- apps/app-front/src/App.tsx
+++ apps/app-front/src/App.tsx
@@ -1,11 +1,38 @@
-import { APP_FRONT_PROD_URL } from '@stats-station/constants';
+import { useEffect } from 'react';
 
 const App = () => {
+  const twitchToken = document.location.hash.split('&')[0].split('=')[1];
+
+  const handleClick = () => {
+    const twitchClientId = import.meta.env.VITE_TWITCH_CLIENT_ID;
+    const redirectUri = 'http://localhost:5173';
+    const scopes = [
+      'user:read:email',
+      'channel:read:goals',
+      'channel:read:polls',
+      'user:read:subscriptions',
+      'channel:read:predictions'
+    ].join('+');
+
+    const twitchAuthUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${twitchClientId}&redirect_uri=${redirectUri}&response_type=token&scope=${scopes}`;
+    window.location.href = twitchAuthUrl;
+  };
+
+  useEffect(() => {}, []);
+
   return (
-    <>
-      <p>app-front</p>
-      <p>url: {APP_FRONT_PROD_URL}</p>
-    </>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '100vh'
+      }}
+    >
+      <button onClick={handleClick}>Twitch auth</button>
+      {twitchToken && <p>{twitchToken}</p>}
+    </div>
   );
 };
 
