8a0e0dc61c7fb6e6c6e69d78143cd655cd4ccbb0
Ugo PRENAT <ugo.prenat@edu.itescia.fr>
2023-12-23 10:08:23 +0100

    refactor: rename helpers

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -6,7 +6,7 @@ export * from './fetchers/twitchFetcher';
 export * from './fetchers/twitterFetcher';
 export * from './fetchers/fetcher.utils';
 
-export * from './utils';
+export * from './helpers/helpers.utils';
 
 export * from './validators/validators';
 export * from './validators/validators.models';
