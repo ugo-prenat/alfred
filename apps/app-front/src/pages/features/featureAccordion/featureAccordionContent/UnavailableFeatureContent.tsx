0ad9dd2c4aee76c3cd8e34c062490c9e8e2340e4
Ugo PRENAT <ugo.prenat@edu.itescia.fr>
2024-01-21 19:29:44 +0100

    fix: feature content text

--- apps/app-front/src/pages/features/featureAccordion/featureAccordionContent/UnavailableFeatureContent.tsx
+++ apps/app-front/src/pages/features/featureAccordion/featureAccordionContent/UnavailableFeatureContent.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { FeatureName } from '@alfred/models';
+import { useFeature } from '@pages/features/features.hooks';
 
 interface IUnavailableFeatureContentProps {
   featureName: FeatureName;
@@ -7,14 +8,21 @@ interface IUnavailableFeatureContentProps {
 }
 
 const UnavailableFeatureContent: FC<IUnavailableFeatureContentProps> = ({
+  featureName,
   botIsNotLinked = false
 }) => {
+  const feature = useFeature(featureName);
+
   return (
     <div className="flex flex-col p-6 pt-0">
       <p>je suis indisponible</p>
       <p>
         cause :{' '}
-        {botIsNotLinked ? "le bot n'est pas configuré" : 'feature coming-soon'}
+        {botIsNotLinked
+          ? "le bot n'est pas configuré"
+          : feature.type === 'eventSub' && !feature.featureActivatedOnTwitch
+            ? 'feature pas activé sur Twitch'
+            : 'feature coming-soon'}
       </p>
     </div>
   );
