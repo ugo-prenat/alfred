f386806a693a8ca4bd9bad2f99d37c2491b8828f
Ugo PRENAT <ugo.prenat@edu.itescia.fr>
2024-01-28 11:54:00 +0100

    refactor: remove unnecessary env var

--- apps/hub/src/services/twitch/twitch.routes.ts
+++ apps/hub/src/services/twitch/twitch.routes.ts
@@ -1,10 +1,6 @@
 import { Hono } from 'hono';
 import { handleTwitchWebhook, twitchWebhookAuth } from './twitch.middleware';
 import { createEventSubSubscription } from './twitch.eventsub';
-import {
-  handleGetBroadcasterFollowers,
-  handleGetBroadcasterSubscribers
-} from './twitch.broadcasters';
 import { basicAuth } from '@/utils/auth.utils';
 
 const twitchRoute = new Hono();
@@ -12,14 +8,5 @@ twitchRoute.use('/broadcasters/*', basicAuth);
 
 twitchRoute.post('/eventsub', twitchWebhookAuth, handleTwitchWebhook);
 twitchRoute.post('/eventsub/subscriptions', createEventSubSubscription);
-
-twitchRoute.get(
-  '/broadcasters/:broadcasterId/subscribers',
-  handleGetBroadcasterSubscribers
-);
-twitchRoute.get(
-  '/broadcasters/:broadcasterId/followers',
-  handleGetBroadcasterFollowers
-);
 
 export default twitchRoute;
